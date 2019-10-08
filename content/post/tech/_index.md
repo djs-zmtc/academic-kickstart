ba5079059afca9018f9a7d32e4cdf04d9f78ed1d
Douglas Sweeney (STARFURY) <douglas.sweeney@zmtcreative.com>
2019-10-08 18:21:13 -0400

    add toc expand/collapse, and related css/js

---
+++ content/post/tech/_index.md
@@ -0,0 +1,16 @@
+---
+title: Tech Topics
+
+# View.
+#   1 = List
+#   2 = Compact
+#   3 = Card
+view: 2
+reading_time: false
+nometa: true
+
+# Optional header image (relative to `static/img/` folder).
+header:
+  caption: ""
+  image: ""
+---
--- themes/academic-zmtc/assets/js/custom.js
+++ themes/academic-zmtc/assets/js/custom.js
@@ -12,4 +12,29 @@ $(window).scroll(function() {
     $('html, body').animate({scrollTop: 0}, 1000);
     return false;
   });
-  
+
+  $('#toc-expand').click(function() {
+    // var newstyle = document.createElement('style');
+    var group, i, tocexp, toccol;
+    group = document.querySelectorAll('#TableOfContents ul ul');
+    tocexp = document.querySelector('#toc-expand');
+    toccol = document.querySelector('#toc-collapse');
+    for (i = 0; i < group.length; ++i) {
+      group[i].style.display = 'block';
+    }
+    tocexp.style.display = 'none';
+    toccol.style.display = 'inline';
+  });  
+
+  $('#toc-collapse').click(function() {
+    // var newstyle = document.createElement('style');
+    var group, i, tocexp, toccol;
+    group = document.querySelectorAll('#TableOfContents ul ul');
+    tocexp = document.querySelector('#toc-expand');
+    toccol = document.querySelector('#toc-collapse');
+    for (i = 0; i < group.length; ++i) {
+      group[i].style.display = 'none';
+    }
+    tocexp.style.display = 'inline';
+    toccol.style.display = 'none';
+  });  
